b37f864da8d2bd03cb6a9a5bc71d24eda0ae90e0
rezah65 <HOSSEINI.REZA65@GMAIL.COM>
2025-04-17 18:41:54 +0330

    Customize Django admin for CustomUser model

--- src/quicksign/apps/users/admin.py
+++ src/quicksign/apps/users/admin.py
@@ -1,12 +1,43 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('phone_number', 'email', 'first_name', 'last_name')
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('phone_number', 'email', 'first_name', 'last_name')
-    list_filter = ('is_staff', 'is_active', 'created_at', 'updated_at')
-    search_fields = ('phone_number', 'email')
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ('phone_number', 'email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_active', 'created_at')
+    search_fields = ('phone_number', 'email', 'first_name', 'last_name')
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important Dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
